e798275a8fb4dbd913296a03bebfcef72a2f8378
David Morales <morgar.creative@gmail.com>
2017-12-10 09:08:48 -0600

    MySQL Database, JSON files created. Installed NPM MySQL and Inquirer packages.

---
+++ bamazondb.sql
@@ -0,0 +1,11 @@
+SELECT * FROM bamazondb.products;
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+
+VALUES ('65" Smart TV', 'Technology', 1200.60, 30),
+				('Watermelon', 'Food', 2.30, 10),
+        ('Lego Drone', 'Toys', 32.70, 20),
+        ('Nivea Body Lotion', 'Personal Care', 2.60, 50),
+        ('Skates', 'Toys', 26.45, 20),
+        ('Canon Printer', 'Teachnology', 64.00, 10),
+        ('Motor Oil', 'Auto', 4.62, 600)
